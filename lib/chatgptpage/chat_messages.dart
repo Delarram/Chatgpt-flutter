372deea635c473fe9b14b5b656eed008dc30febe
Delarram <tunyanaing960@gmail.com>
2023-01-18 01:39:36 +0630

    refactor

--- lib/chatgptpage/chat_messages.dart
+++ lib/chatgptpage/chat_messages.dart
@@ -8,18 +8,21 @@ class Messages extends StatelessWidget {
   final String text;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(radius: 50,backgroundColor: Colors.white,child: Text(sender[0]),),
-        Expanded(
-          child: Column(
-            children: [
-              Text(sender,style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500,),),SizedBox(height:5,),
-              Text(text,style: TextStyle(fontSize: 14),)
-            ],
-          ),
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          CircleAvatar(radius: 50,backgroundColor: Colors.white,child: Text(sender[0]),),
+          Expanded(
+            child: Column(
+              children: [
+                Text(sender,style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500,),),SizedBox(height:5,),
+                Text(text,style: TextStyle(fontSize: 14),)
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
